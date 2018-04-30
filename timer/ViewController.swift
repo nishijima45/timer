3e7532cce39005927a7f9e500f2e28e22cace8be
西嶋信吾 <tree-frog@live.jp>
2018-04-30 19:47:13 +0900

    タイマーを設定してボタンを配置 シミュレータで起動すると、再生ボタンを押すほど時計が早くなる、リセットボタンでリセット、 は授業通りだが、一時停止ボタンが機能していない

--- timer/ViewController.swift
+++ timer/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    // タイマー用の時間のための変数
+    //ボタンを実装する際に追加
+    var timer: Timer!
+    var timer_sec: Float = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    // selector: #selector(updatetimer) で指定された関数
+    // timeInterval: 0.1, repeats: true で指定された通り、0.1秒毎に呼び出され続ける
+    @objc func updateTimer(timer: Timer) {
+        self.timer_sec += 0.1
+        self.timerLabel.text = String(format: "%.1f", timer_sec)
+    }
+    
+    
+    @IBAction func startTimer(_ sender: Any) {
+        // 再生ボタンを押すとタイマー作成、始動
+        self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func pauseTimer(_ sender: Any) {
+        // タイマーを破棄
+        self.timer.invalidate()
+    }
+    
+    
+    @IBAction func resetTimer(_ sender: Any) {
+        // リセットボタンを押すと、タイマーの時間を0に
+        self.timer_sec = 0
+        self.timerLabel.text = String(format: "%.1f", self.timer_sec)
+}
 }
 
